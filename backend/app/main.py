339ab73ffc37975f5cb57f9df05623663c52ddf5
Juan Montenegro <ju4niko@gmail.com>
2026-05-26 15:57:29 -0300

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from pathlib import Path
 
 import pandas as pd
@@ -13,6 +15,19 @@ from .models import System
 from typing import Optional
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+)
 
 # crea tablas automáticamente
 Base.metadata.create_all(bind=engine)
